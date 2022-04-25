c6fbf75f6a8311fd9e56551cf7f51fa47841844e
Vibha Masti <vsmasti@gmail.com>
2022-04-25 04:07:15 +0000

    Fixing bugs

--- src/DatabaseUtilities.java
+++ src/DatabaseUtilities.java
@@ -10,7 +10,7 @@ abstract class DatabaseUtilities
     private static String user;
     private final String password;
 
-    private String tableName;
+    protected String tableName;
 
     Connection conn;
     DatabaseUtilities(String tableName)
